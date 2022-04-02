95d152e04366b213140fb1208dcb1b07f4d81c58
Amelia Deng <aden9801@uni.sydney.edu.au>
2022-04-03 02:16:04 +1100

    morsecode conversion fix

--- script.js
+++ script.js
@@ -1,23 +1,10 @@
-// // Import functions
-// import { mcToEng } from "./morsecode.js";
-// import { engToMc } from "./morsecode.js";
-
-// selecting both of my tests
-const input = document.querySelector("#morseInput");
-const output = document.querySelector("#morseOutput");
+// DOM INTERACTION
+const english = document.querySelector("#morseInput");
+const morsecode = document.querySelector("#morseOutput");
 const switchModes = document.querySelector("#switchModes");
 const resetBtn = document.querySelector("#resetBtn");
 
-// DUE ON TUESDAY 5th of APRIL
-// ### MVP:
-// Challenge 1: Morse Code Project
-// 1. Create a user interface that allows the user to either input some English text or some Morse Code
-// 1. Create JS functions that would allow the user to translate their English text into Morse Code or Morse Code into English text
-// 1. Make sure to handle spaces properly (ie. there is 1 space between English words, but one space between Morse Code characters)
-// I will give a deadline tomorrow once all the calculator have been submitted but if you are already done with the calculator you can start working on this
-
-// Have two functions that one morse to english and the other way around
-// Translate Object Example
+// BUTTON THAT SHOULD DISABLE THE OTHER TEXT AREA
 const buttonClicked = () => {
     !input.disabled
         ? ((input.disabled = true), (output.disabled = false))
@@ -75,56 +62,96 @@ const morseCodeDictionary = {
     " ": " ",
 };
 
-// Reversing key and value in morsecodedictionary and storing it in a new object => morsecode : "alphabet,numbers and punctuation"
-const morsecode = Object.entries(morseCodeDictionary).reduce(
+// Reversing key and value in morsecodedictionary and storing it in a new object => "alphabet,numbers and punctuation" : morsecode
+const alphabetAndNum = Object.entries(morseCodeDictionary).reduce(
     (object, [key, value]) => {
         object[value] = key;
         return object;
     },
     {}
 );
-console.log(morsecode);
+console.log(alphabetAndNum);
 
-// match input.value to our constant morsecode and we return that into our output.value
-// split word into seperate letter, map each letter to its specific key in the morsecode depending on the letter, and return its value
+// Have two functions => convert english to morse / convert morse to english => attach to seperate eventListeners
+
 // key:value = {[0],[1]}
-const englishToMorseCode = () => {
-    // got the alphabets stuck together
-    const engToMc = input.value
-        .toLowerCase()
-        .split("")
-        .map((letter) => {
-            return Object.entries(morsecode).find(
-                (key) => key[0] === letter
-            )[1];
-        })
-        .join(" ");
-    console.log(engToMc);
-    output.value = engToMc;
-};
-console.log(englishToMorseCode());
 
-input.addEventListener("input", englishToMorseCode);
+/* 
+thought process =>
+- input.value converted to lowercase to match our keys 
+- replace space using regex with a / => "Hello There" = /......-...-..---/-......-../
+- split the string into individual arrays by / => /......-...-..---/-......-../ = ['hello', 'there']
+- Map through each word and split it into individual letters => ['hello', 'there'] = ['h', 'e', 'l', 'l', 'o']
+ ['t', 'h', 'e', 'r', 'e']
+- Map through each letter and return the matching morsecode based on its key => ['hello', 'there'] = ['h', 'e', 'l', 'l', 'o']['t', 'h', 'e', 'r', 'e'] => ['....', '.', '.-..', '.-..', '---'] ['-', '....', '.', '.-.', '.']
+- Join the letters together to make a single word =>  ['......-...-..---', '-......-..',]
+- Join the words together with a /
+*/
+
+const englishToMorseCode = () => {
+    const engToMc = english.value
+        .toLowerCase()
+        .replace(/\s{1,}/g, "/")
+        .split("/")
+        .map((word) => {
+            return word
+                .split("")
+                .map((letter) => {
+                    return Object.entries(alphabetAndNum).find(
+                        (key) => key[0] === letter
+                    )[1];
+                })
+                .join("");
+        })
+        .join("/");
+    morsecode.value = engToMc;
+};
+
+english.addEventListener("input", englishToMorseCode);
+
+// THERE IS A SPACE BETWEEN MORSE DOTS/DASHES FOR EACH LETTER
+/* 
+thought process =>
+- split the morse code by its / => (Hello there) .... . .-.. .-.. --- / - .... . .-. . = ['.... . .-.. .-.. --- ', ' - .... . .-. .']
+- Map through each morsecodeword, trim leading spaces before and after the word and then split the word into each individual morsecode letter => ['....', '.', '.-..', '.-..', '---'] ['-', '....', '.', '.-.', '.'] = ['....', '.', '.-..', '.-..', '---'] ['-', '....', '.', '.-.', '.']
+- Map through each letter and return the matching alphabet/num/punctuation based on its key = ['....', '.', '.-..', '.-..', '---'] ['-', '....', '.', '.-.', '.'] = ['h', 'e', 'l', 'l', 'o'] ['t', 'h', 'e', 'r', 'e']
+- if letter is empty return an empty string otherwise if the morsecode input does not register a similar key, the value will return as "X"
+- Join the letters together => ['hello', 'there']
+- Join the words together => Hello There
+*/
 
 const morsecodeToEnglish = () => {
-    const mcToEng = output.value
-        .toLowerCase()
-        .split("")
-        .map((letter) => {
-            return Object.entries(morseCodeDictionary).find(
-                (key) => key[0] === letter
-            )[1];
+    const mcToEng = morsecode.value
+        .trim()
+        .split("/")
+        .map((morseCodeWord) => {
+            let morseCodeLetter = morseCodeWord
+                .trim()
+                .split(" ")
+                .map((dotDash) => {
+                    if (dotDash === "") {
+                        return "";
+                    }
+                    const letter = Object.entries(morseCodeDictionary).find(
+                        (key) => key[0] === dotDash
+                    );
+                    console.log(letter);
+                    return letter ? letter[1] : "X";
+                });
+            console.log(morseCodeLetter);
+            return morseCodeLetter.join("");
         })
         .join(" ");
     console.log(mcToEng);
-    input.value = mcToEng;
+    english.value = mcToEng;
 };
-output.addEventListener("input", morsecodeToEnglish);
+
+morsecode.addEventListener("input", morsecodeToEnglish);
 
 // CLEAR INPUT FIELDS
 const clearAll = () => {
-    input.value = "";
-    output.value = "";
+    english.value = "";
+    morsecode.value = "";
 };
 
 resetBtn.addEventListener("click", clearAll);
